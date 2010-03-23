f0d49cc8cc30a5129c2e8d0e32c4f892f60185bc
srobarts <srobarts@e308f7b1-9826-4704-957f-b109f8601d91>
2010-03-23 22:35:02 +0000

    test
    git-svn-id: https://blinkdesigns.svn.beanstalkapp.com/timecards/trunk@3 e308f7b1-9826-4704-957f-b109f8601d91

--- app/index.php
+++ app/index.php
@@ -7,7 +7,7 @@
  * Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
  *
  * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
+ * Redistributions of file must retain the above copyright notice.
  *
  * @filesource
  * @copyright     Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
